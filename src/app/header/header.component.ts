7c1d77d485da399bcffae6c2affc0d362e75c7c0
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-04-05 13:26:58 +0700

    add more menu

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,27 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 const PAGELIST: Array<any> = [
+  {routerLink: 'accordion', menuName: 'Accordion'},
   {routerLink: 'autocomplete', menuName: 'Auto Complete'},
   {routerLink: 'badge', menuName: 'Badge'},
   {routerLink: 'button', menuName: 'Button'},
   {routerLink: 'chart', menuName: 'Chart'},
   {routerLink: 'checkbox', menuName: 'Check Box'},
+  {routerLink: 'datagrid', menuName: 'Datagrid'},
+  {routerLink: 'datatable', menuName: 'Datatable'},
   {routerLink: 'datetimepicker', menuName: 'Datetime Picker'},
+  {routerLink: 'dialog', menuName: 'Dialog'},
+  {routerLink: 'galleria', menuName: 'Galleria'},
   {routerLink: 'imagegallery', menuName: 'Image Gallery'},
-  {routerLink: 'popup', menuName: 'Popup'},
+  {routerLink: 'panel', menuName: 'Panel'},
   {routerLink: 'progressbar', menuName: 'Progress Bar'},
-  {routerLink: 'pulldown', menuName: 'Pulldown Menu'},
+  {routerLink: 'pulldownmenu', menuName: 'Pulldown Menu'},
   {routerLink: 'radiobutton', menuName: 'Radio Button'},
   {routerLink: 'selectbox', menuName: 'Select Box'},
   {routerLink: 'sidebar', menuName: 'Side Bar'},
-  {routerLink: 'tab', menuName: 'Tab'},
+  {routerLink: 'tabview', menuName: 'Tabview'},
   {routerLink: 'table', menuName: 'Table'},
   {routerLink: 'textarea', menuName: 'Text Area'},
   {routerLink: 'textbox', menuName: 'Text Box'},
   {routerLink: 'texteditor', menuName: 'Text Editor'},
-  {routerLink: 'tooltip', menuName: 'Tool Tip'},
+  {routerLink: 'tooltip', menuName: 'Tooltip'},
   {routerLink: 'tree', menuName: 'Tree'},
   {routerLink: 'upload', menuName: 'Upload'},
+
 ];
 
 @Component({
