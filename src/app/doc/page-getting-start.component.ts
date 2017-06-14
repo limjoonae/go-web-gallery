c8dfeb0bbf54f791e481b020af743e2202157aec
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-06-14 18:09:16 +0700

    add download link and update installation step

--- src/app/doc/page-getting-start.component.ts
+++ src/app/doc/page-getting-start.component.ts
@@ -14,27 +14,28 @@ import { Component, Input } from '@angular/core';
 })
 export class GettingStartComponent  { 
 
-    releaseDate:string = '12 May 2017';
+    releaseDate:string = '14 June 2017';
     prerequisiteInstall: Array<any> = [
         {},
     ];
     setupNodejs:Array<any> = [
-        { data: `2.	ทำการติดตั้ง Node.js เมื่อทำการติดตั้ง Node.js จะได้ Node และตัว npm (Node Package Manager) ซึ่งเป็นตัวจัดการ package/module ต่าง ๆ ของ Node มาด้วย`},
-        { code:`node –v`, data: `3.	ตรวจสอบว่าเราทำการติดตั้ง Node.js สมบูรณ์หรือไม่ โดยการเช็คเวอร์ชันด้วยคำสั่งนี้`},
-        { code:`npm -v`, data: `4.	ตรวจเช็คเวอร์ชัน npm `},
+        { data: `1.	ทำการติดตั้ง Node.js เมื่อทำการติดตั้ง Node.js จะได้ Node และตัว npm (Node Package Manager) ซึ่งเป็นตัวจัดการ package/module ต่าง ๆ ของ Node มาด้วย`},
+        { code:`node –v`, data: `2.	ตรวจสอบว่าเราทำการติดตั้ง Node.js สมบูรณ์หรือไม่ โดยการเช็ค version ด้วยคำสั่งนี้`},
+        { code:`npm -v`, data: `3.	ตรวจเช็ค version npm `},
     ];
 
     setupGit:Array<any> = [
-        // image: `image/setupGit-4.jpg`,
-        { data: `3.	หลังจากติดตั้ง Git เรียบร้อย สามารถตรวจสอบ version ของ git ได้ โดยใน Git GUI สามารถดูได้ที่เมนู Help > About Git Gui`},
+        { data: `1. ติดตั้ง Git โดยคลิกขวาที่ installer เลือก Run as administrator แล้วติดตั้งตาม Default Setting ทั้งหมด (ดูวิธีการติดตั้งเพิ่มเติมที่ <a href="http://www.artit-k.com/dev-install-git-for-windows" target="_blank" ><strong>ลิ้งค์นี้</strong></a>)`},
+        { data: `2.	หลังจากติดตั้ง Git เรียบร้อย สามารถตรวจสอบ version ของ git ได้ โดยใน Git GUI สามารถดูได้ที่เมนู Help > About Git Gui`},
         { code:`git version หรือ git --version`, data: `ส่วนของ Git bash จะเป็นหน้า Command Prompt สามารถตรวจสอบ version ได้ด้วยการพิมพ์คำสั่ง`},
     ];
 
     createProjectAngular:Array<any> = [
         { data: `1. เปิด command prompt แล้วไปยัง path ที่ต้องการจะสร้างโปรเจค แล้วทำการ clone โปรเจคตั้งต้น ด้วยคำสั่งนี้`, code: `git clone http://10.182.247.74/go-webframework/go-starter-project.git ชื่อโปรเจค`},
-        { data: `2. เปิด command prompt ขึ้นมาใน root path project แล้วรันคำสั้ง` , code: `npm --proxy http://aduser:adpass@10.182.255.166:8080 install
+        { data: `2. เปิด command prompt ขึ้นมาใน root path project แล้วรันคำสั่ง` , code: `npm --proxy http://aduser:adpass@10.182.255.166:8080 install
   หมายเหตุ aduser = user lan, adpass = password ของ user lan`},
         { data: `3. สตาร์ทโปรเจคด้วยคำสั่ง`, code: `npm start`},
+        { data: `browser จะถูกเปิดอัตโนมัติ และแสดงคำว่า Hello Angular`},
     ];
 
     setProxy: Array<any> = [
@@ -45,7 +46,7 @@ export class GettingStartComponent  {
     
 
     howToSystemjs:Array<any> = [
-        { code: `'npm:': 'http://10.182.247.73/go-cdn-dev/node_modules/4.0.0/',`, data: `1.	กำหนด path ที่ชื่อ npm เพื่อเรียกใช้งาน library หลักของ Angular2 ดังนี้`},
+        { code: `'npm:': 'http://10.182.247.73/go-cdn-dev/node_modules/4.0.0/',`, data: `1.	กำหนด path ที่ชื่อ npm เพื่อเรียกใช้งาน library หลักของ Angular ดังนี้`},
         { code: `'go:': 'http://10.182.247.73/go-cdn-dev/dist/go/1.0.0/',`, data: `2.	กำหนด path ที่ชื่อ go เพื่อเรียกใช้งาน component ดังนี้`},
         { code: `'lib:': 'http://10.182.247.73/go-cdn-dev/dist/lib/1.0.0/'`, data: `3.	กำหนด path ที่ชื่อ lib เพื่อเรียกใช้งาน component ดังนี้`},
         { data: `4. วาง path ทั้ง 2 ลงใน paths`},
