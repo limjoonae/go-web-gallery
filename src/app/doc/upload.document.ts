9294aaf750891397522049c296fe858455a019e0
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-07-06 12:44:18 +0700

    update upload document

--- src/app/doc/upload.document.ts
+++ src/app/doc/upload.document.ts
@@ -3,17 +3,17 @@ import { Component, OnInit } from '@angular/core';
 const ATTRIBUTELIST: Array<any> = [
     { require: '*', name: 'name', type: 'string', description: `กำหนดชื่อของ component` },
     { require: '*', name: 'url', type: 'string', description: `กำหนด path ที่จะนำไฟล์อัพโหลดขึ้นไป` },
-    { require: '', name: 'multiple', type: 'boolean', description: `กำหนดจำนวนไฟล์ที่ต้องการอัพโหลดว่าสามารถอัพโหลดได้ทีละไฟล์หรือหลายไฟล์พร้อมกัน` },
+    { require: '', name: 'multiple', type: 'boolean', description: `ใช้กำหนดว่าให้สามารถอัพโหลดได้ทีละหลายไฟล์พร้อมกันหรือไม่ มีค่า default เป็น true` },
     { require: '', name: 'accept', type: 'string', description: `กำหนดประเภทของไฟล์ที่แสดงตอนเลือกไฟล์ โดยสามารถดูได้จาก <strong><a href="http://www.iana.org/assignments/media-types/media-types.xhtml" target="_blank">ลิงค์นี้</a></strong>` },
-    { require: '', name: 'fileTypeAllow', type: 'string', description: `กำหนดประเภทของไฟล์ที่สามารถเลือกเพื่อที่จะอัพโหลดได้ โดยสามารถดูได้จาก <strong><a href="http://www.iana.org/assignments/media-types/media-types.xhtml" target="_blank">ลิงค์นี้</a></strong>` },
-    { require: '', name: 'autoUpload', type: 'boolean', description: `กำหนดให้สามารถทำการอัพโหลดไฟล์อัตโนมัติได้ เมื่อทำการเลือกไฟล์เสร็จสิ้น` },
-    { require: '', name: 'maxFileSize', type: 'number', description: `กำหนดขนาดสูงสุดของแต่ละไฟล์ที่สามารถอัพโหลดได้ โดยให้กำหนดเป็นขนาดของ bytes (กำหนดค่าเริ่มต้นไว้ที่ 2 MB)` },
-    { require: '', name: 'showImage', type: 'boolean', description: `กำหนดให้แสดงภาพตัวอย่างของไฟล์ที่เป็นประเภทภาพที่เลือกไว้` },
+    { require: '', name: 'fileTypeAllow', type: 'string', description: `กำหนดประเภทของไฟล์ที่สามารถอัพโหลดได้ โดยสามารถดูได้จาก <strong><a href="http://www.iana.org/assignments/media-types/media-types.xhtml" target="_blank">ลิงค์นี้</a></strong>` },
+    { require: '', name: 'autoUpload', type: 'boolean', description: `กำหนดให้สามารถทำการอัพโหลดไฟล์อัตโนมัติได้ เมื่อทำการเลือกไฟล์เสร็จสิ้น มีค่า default เป็น false` },
+    { require: '', name: 'maxFileSize', type: 'number', description: `กำหนดขนาดสูงสุดของแต่ละไฟล์ที่สามารถอัพโหลดได้ (มีค่าเริ่มต้นเป็น 2 MB) รูปแบบการกำหนดค่าคือ 2 * 1024 * 1024 (ได้ค่าเป็น bytes)` },
+    { require: '', name: 'showImage', type: 'boolean', description: `กำหนดให้แสดงภาพตัวอย่างของไฟล์ที่เป็นประเภทภาพที่เลือกไว้ มีค่า default เป็น true` },
     { require: '', name: 'previewWidth', type: 'number', description: `กำหนดความกว้างของภาพตัวอย่างที่แสดง ซึ่งจะใช้งานเมื่อ showImage ถูกเปิดใช้งานอยู่(กำหนดค่าเริ่มต้นไว้ที่ 100)` },
-    { require: '', name: 'speedCalculationTime', type: 'number', description: `ใช้กำหนดเวลา(ms)ของการคำนวณความเร็วอัพโหลดในแต่ละรอบ` },
-    { require: '', name: 'canAbort', type: 'boolean', description: `กำหนดให้สามารถยกเลิกการอัพโหลดไฟล์ได้` },
-    { require: '', name: 'messageSettings', type: 'Object', description: `ใช้กำหนดคำอธิบายต่างๆของตัว component` },
-    { require: '', name: 'showOptions', type: 'Object', description: `ใช้กำหนดการแสดงของสิ่งต่างๆบน component` },
+    { require: '', name: 'speedCalculationTime', type: 'number', description: `ใช้กำหนดเวลาของการคำนวณความเร็วอัพโหลดในแต่ละรอบ หน่วยเป็น millisecond(ms) มีค่า default เป็น 500` },
+    { require: '', name: 'canAbort', type: 'boolean', description: `กำหนดให้สามารถยกเลิกการอัพโหลดไฟล์ได้ มีค่า default เป็น true` },
+    { require: '', name: 'messageSettings', type: 'Object', description: `ใช้กำหนด label ของปุ่ม และ message ต่างๆของ component ค่าที่สามาถกำหนดได้ดูได้จาก <strong><a href="#messageSettingsTopic">messageSettings Object</a></strong>` },
+    { require: '', name: 'showOptions', type: 'Object', description: `ใช้กำหนดการแสดงผลของสิ่งต่างๆบน component ค่าที่สามาถกำหนดได้ดูได้จาก <strong><a href="#showOptionsTopic">showOptions Object</a></strong>` },
     { require: '', name: 'onSelect', type: 'function($event)', description: `ฟังก์ชั่นนี้จะถูกเรียกใช้ เมื่อมีการเลือกไฟล์` },
     { require: '', name: 'onDelete', type: 'function($event)', description: `ฟังก์ชั่นนี้จะถูกเรียกใช้ เมื่อมีการลบไฟล์ที่เลือกไว้` },
     { require: '', name: 'onBeforeUpload', type: 'function($event)', description: `ฟังก์ชั่นนี้จะถูกเรียกใช้ เมื่อมีกำลังจะทำการอัพโหลดไฟล์` },
@@ -38,13 +38,18 @@ const MSGATTRIBUTELIST: Array<any> = [
 ];
 
 const SHOWATTRIBUTELIST: Array<any> = [
-    { require: '', name: 'showAllFileChosen', type: 'boolean', description: `กำหนดให้แสดงชื่อไฟล์ที่ถูกเลือก` },
-    { require: '', name: 'showProgressBar', type: 'boolean', description: `กำหนดให้แสดงแถบสถานะการอัพโหลด` },
-    { require: '', name: 'showLatestFileNameChosen', type: 'boolean', description: `กำหนดให้แสดงชื่อไฟล์ที่ถูกเลือกล่าสุด` },
-    { require: '', name: 'showBrowseButton', type: 'boolean', description: `กำหนดให้แสดงปุ่มเลือกไฟล์` },
-    { require: '', name: 'showInvalidFileSizeError', type: 'boolean', description: `กำหนดให้แสดงข้อความผิดพลาดเมื่อไฟล์ที่เลือกมีขนาดไม่ถูกต้อง` },
-    { require: '', name: 'showDragandDropBox', type: 'boolean', description: `กำหนดให้แสดงกล่อง Drag and Drop` },
-    { require: '', name: 'showUploadStatusDialog', type: 'boolean', description: `กำหนดให้แสดงข้อความเมื่ออัพโหลดสำเร็จหรือล้มเหลว` }
+    { require: '', name: 'showAllFileChosen', type: 'boolean', description: `กำหนดให้แสดงชื่อไฟล์ที่ถูกเลือก มีค่า default เป็น true` },
+    { require: '', name: 'showProgressBar', type: 'boolean', description: `กำหนดให้แสดงแถบสถานะการอัพโหลด มีค่า default เป็น true` },
+    { require: '', name: 'showLatestFileNameChosen', type: 'boolean', description: `กำหนดให้แสดงชื่อไฟล์ที่ถูกเลือกล่าสุด มีค่า default เป็น true` },
+    { require: '', name: 'showBrowseButton', type: 'boolean', description: `กำหนดให้แสดงปุ่มเลือกไฟล์ มีค่า default เป็น true` },
+    { require: '', name: 'showInvalidFileSizeError', type: 'boolean', description: `กำหนดให้แสดงข้อความเตือนเมื่อไฟล์ที่เลือกมีขนาดไม่ถูกต้อง มีค่า default เป็น true` },
+    { require: '', name: 'showDragandDropBox', type: 'boolean', description: `กำหนดให้แสดงกล่อง Drag and Drop file มีค่า default เป็น false` },
+    { require: '', name: 'showUploadStatusDialog', type: 'boolean', description: `กำหนดให้แสดงข้อความเมื่ออัพโหลดสำเร็จหรือล้มเหลว มีค่า default เป็น true` }
+];
+
+const _releaseUpdate_v_1_1: Array<any> = [
+  { head: ``, data: `ปรับปรุงรูปแบบการแสดงผลไฟล์ที่ถูกเลือก` },
+  { head: ``, data: `ปรับปรุงรูปแบบการแสดงข้อความในกล่องแสดง error mesaage` },
 ];
 
 @Component({
@@ -54,9 +59,9 @@ const SHOWATTRIBUTELIST: Array<any> = [
 export class UploadDocument implements OnInit {
 
     private componentTag: string = '<go-upload>';
-    private componentDescription: string = `ใช้สำหรับอัพโหลดไฟล์ขึ้นไปบน path ที่ต้องการ`;
-    private version: string = '1.0';
-    private releaseDate: string = '13/1/2017';
+    private componentDescription: string = `ใช้สำหรับอัพโหลดไฟล์ประเภทต่างๆขึ้นไปบน path ที่ต้องการ`;
+    private version: string = '1.1';
+    private releaseDate: string = '6 July 2017';
     private prefixSyntax: string = `<go-upload `;
     private attrSyntax: string = `name="component_name" url="upload_url_path"
      [[multiple]="is_multiple_upload"] [accept="file_type_accepted"] 
@@ -71,6 +76,8 @@ export class UploadDocument implements OnInit {
      [(onUploading)="onUploading($event)"] [(onFinish)="onFinish($event)"] 
      [(onError)="onError($event)"] [(onClear)="onClear($event)"]`;
     private suffixSyntax: string = `></go-upload>`;
+
+    private releaseUpdate_v_1_1 = _releaseUpdate_v_1_1
 
     private messageSettingsSyntax: string = `constDATA: messageSettings{} = 
     { invalidFileSizeMsg:'invalid_file_size_message', 
@@ -98,24 +105,100 @@ export class UploadDocument implements OnInit {
     private appModuleDetail: string = `app.module.ts`;
     private systemJSDetail: string = `systemjs.config.js`;
 
-    uploadedFiles: any[] = [];
     status: string = '';
     private uploadedFileMessage = "รายชื่อไฟล์ที่อัพโหลดเสร็จสิ้น";
     private exampleDescription = "<h5>สำหรับตัวอย่างนี้ จะแบ่งออกเป็น 2 ส่วนได้แก่</h5> <p><strong>1.ส่วนด้านใน </strong>โดยจะประกอบด้วยส่วนที่ใช้เลือกไฟล์เพื่ออัพโหลดซึ่งได้มีการเตรียมไว้ให้<i>(รวมถึงการแสดงรายชื่อไฟล์เพื่อจะทำการอัพโหลด)</i></p> <p><strong>2.ส่วนด้านนอก </strong>โดยในส่วนนี้ จะเป็น<strong>\"รายชื่อไฟล์ที่อัพโหลดเสร็จสิ้น\"</strong>ซึ่งจะรับค่ามาจาก output ของส่วนใน component</p>";
 
-    private code: string = `<go-upload name="demo" id="upload_ex" url="http://localhost:10050/upload" 
-    [multiple]="true" accept="image/*,text/*" fileTypeAllow="image/*,text/*" 
-    [autoUpload]="false" [maxFileSize]=50*1024*1024 [showImage]="true" 
-    [previewWidth]="100" [speedCalculationTime]=1000 [canAbort]="true" 
-    [messageSettings]="message_sets" [showOptions]="show_opts" 
-    (onFinish)="onFinish($event)" (onError)="onError($event)"
-    (onSelect)="onSelect($event)" (onDelete)="onDelete($event)"
-    (onClear)="onClear($event)"></go-upload>`;
+    private html_ex_default: string = `<go-upload name="upload_default" 
+    id="upload_default" url="http://localhost:10050/upload" 
+    accept="image/*,text/*" fileTypeAllow="image/*,text/*"
+    (onFinish)="onFinish($event)" (onSelect)="onSelect($event)" 
+    (onDelete)="onDelete($event)" (onClear)="onClear($event)" 
+    (onError)="onError($event)">
+</go-upload>
+<div class="row" *ngIf="uploadedFiles.length > 0" style="margin-top: 2rem;">
+    <strong>{{uploadedFileMessage}}</strong> <button class="btn btn-danger btn-sm" (click)="clearCompletedList()">Clear</button>
+    <div class="row" *ngFor="let file of uploadedFiles">
+        <div class="col-md-12">
+            <li>{{file.name}} - [{{file.size}} bytes]</li>
+        </div>
+    </div>
+</div>`;
 
-    private msgCode: string = `message_sets = { chooseLabel: "choose me", deleteLabel: "delete it" };`;
-    private showCode: string = `show_opts = { showDragandDropBox: true };`;
+    private ts_ex_default: string = `uploadedFiles: any[] = [];
 
-    private message_sets = { chooseLabel: "choose me", deleteLabel: "delete it" };
+onFinish(event: any) {
+    this.status = 'อัพโหลดสำเร็จ';
+    for (let file of event.files) {
+        this.uploadedFiles.push(file);
+    }
+}
+
+onSelect(event: any) {
+    this.status = 'เลือกไฟล์';
+}
+
+onDelete(event: any) {
+    this.status = 'ลบไฟล์';
+}
+
+onClear(event: any) {
+    this.status = 'ลบไฟล์ทั้งหมด';
+}
+
+onError(event: any) {
+    this.status = 'อัพโหลดผิดพลาด';
+}`;
+
+    private html_ex_upload_set: string = `<go-upload id="upload_set" name="upload_set" url="http://localhost:10050/upload" [multiple]="false" accept="image/*,text/*,application/*" fileTypeAllow="image/*,text/*,application/*"
+    [maxFileSize]=2.5*1024*1024 [showImage]="false" [previewWidth]="100" [speedCalculationTime]=1000
+    [messageSettings]="message_sets" [showOptions]="show_opts" (onFinish)="onFinishSet($event)"
+    (onSelect)="onSelectSet($event)" (onDelete)="onDeleteSet($event)" (onClear)="onClearSet($event)" (onError)="onErrorSet($event)">
+</go-upload>
+<div class="row" *ngIf="uploadedFilesSet.length > 0">
+    <strong>{{uploadedFileMessage}}</strong> <button class="btn btn-danger btn-sm" (click)="clearCompletedList()">Clear</button>
+    <div class="row" *ngFor="let file of uploadedFilesSet">
+        <div class="col-md-12">
+            <li>{{file.name}} - [{{file.size}} bytes]</li>
+        </div>
+    </div>
+</div>`;
+
+    private ts_ex_upload_set: string = `uploadedFilesSet: any[] = [];
+message_sets = { chooseLabel: "Choose file", deleteLabel: "Delete it" };
+show_opts = { showDragandDropBox: true };
+
+onFinishSet(event: any) {
+    this.status = 'อัพโหลดสำเร็จ';
+    for (let file of event.files) {
+        this.uploadedFilesSet.push(file);
+    }
+}
+
+onSelectSet(event: any) {
+    this.status = 'เลือกไฟล์';
+}
+
+onDeleteSet(event: any) {
+    this.status = 'ลบไฟล์';
+}
+
+onClearSet(event: any) {
+    this.status = 'ลบไฟล์ทั้งหมด';
+}
+
+onErrorSet(event: any) {
+    this.status = 'อัพโหลดผิดพลาด';
+}
+
+clearCompletedListSet() {
+    this.uploadedFilesSet = [];
+}`;
+
+    uploadedFiles: any[] = [];
+    uploadedFilesSet: any[] = [];
+
+    private message_sets = { chooseLabel: "Choose file", deleteLabel: "Delete it"};
     private show_opts = { showDragandDropBox: true };
 
     private dlpath = '';
@@ -134,7 +217,6 @@ export class UploadDocument implements OnInit {
 
     onFinish(event: any) {
         this.status = 'อัพโหลดสำเร็จ';
-        //console.log( this.status );
         for (let file of event.files) {
             this.uploadedFiles.push(file);
         }
@@ -142,25 +224,48 @@ export class UploadDocument implements OnInit {
 
     onSelect(event: any) {
         this.status = 'เลือกไฟล์';
-        //console.log( this.status );
     }
 
     onDelete(event: any) {
         this.status = 'ลบไฟล์';
-        //console.log( this.status );
     }
 
     onClear(event: any) {
         this.status = 'ลบไฟล์ทั้งหมด';
-        //console.log( this.status );
     }
 
     onError(event: any) {
         this.status = 'อัพโหลดผิดพลาด';
-        //console.log( this.status );
     }
 
     clearCompletedList() {
         this.uploadedFiles = [];
+    }
+
+    onFinishSet(event: any) {
+        this.status = 'อัพโหลดสำเร็จ';
+        for (let file of event.files) {
+            this.uploadedFilesSet.push(file);
+        }
+    }
+
+    onSelectSet(event: any) {
+        this.status = 'เลือกไฟล์';
+    }
+
+    onDeleteSet(event: any) {
+        this.status = 'ลบไฟล์';
+    }
+
+    onClearSet(event: any) {
+        this.status = 'ลบไฟล์ทั้งหมด';
+    }
+
+    onErrorSet(event: any) {
+        this.status = 'อัพโหลดผิดพลาด';
+    }
+
+    clearCompletedListSet() {
+        this.uploadedFilesSet = [];
     }
 }
