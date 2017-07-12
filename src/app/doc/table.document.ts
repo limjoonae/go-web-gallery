d6acc34ca2d2a250375cd65ebff91df733ffb086
Supakan Siwatmongkol <supakansiw@gosoft.co.th>
2017-07-12 15:01:59 +0700

    update table document

--- src/app/doc/table.document.ts
+++ src/app/doc/table.document.ts
@@ -2,12 +2,12 @@ import { Component } from '@angular/core';
 
 const ATTRIBUTELIST: Array<any> = [
     { require: '', name: 'class', type: 'string', description: `ใช้สำหรับตั้งค่ารูปแบบ table โดยใช้ class ของ bootstrap` },
-    { require: '', name: 'paging', type: 'boolean', description: `ใช้สำหรับตั้งค่าการใช้งานของการจัดหน้าของตาราง` },
-    { require: '', name: 'itemsPerPage', type: 'number', description: `ใช้สำหรับระบุค่าของจำนวนแถวในตารางที่ต้องการแสดงในแต่ละหน้า โดยจะสามารถใช้ได้เมื่อ ‘paging’ ได้เปิดใช้งานเท่านั้น` },
-    { require: '', name: 'maxSize', type: 'number', description: `ใช้สำหรับระบุค่าของจำนวนหน้าที่ต้องการให้แสดงในส่วนของหมายเลขหน้า โดยจะสามารถใช้ได้เมื่อ ‘paging’ ได้เปิดใช้งานเท่านั้น` },
-    { require: '', name: 'showCard', type: 'boolean', description: `ใช้สำหรับตั้งค่าการแสดงของ card ซึ่งใช้แสดงหน้าของ table ที่กำลังดูอยู่ สามารถใช้ได้เมื่อ ‘paging’ ได้เปิดใช้งานเท่านั้น` },
+    { require: '', name: 'paging', type: 'boolean', description: `ใช้สำหรับกำหนดว่าให้มีการแบ่งหน้าของตารางหรือไม่ มีค่า default เป็น true` },
+    { require: '', name: 'itemsPerPage', type: 'number', description: `ใช้สำหรับระบุจำนวน row ข้อมูลที่แสดงใน 1 หน้าตาราง ใช้งานได้เมื่อเปิดใช้งาน paging. มีค่า default เท่ากับ 10` },
+    { require: '', name: 'maxSize', type: 'number', description: `ใช้สำหรับระบุค่าของจำนวนหน้าที่ต้องการให้แสดงในส่วนของหมายเลขหน้า หากกำหนดน้อยกว่าจำนวนหน้าทั้งหมด ปุ่มหน้าอื่นๆจะถูกซ่อนเป็น ... โดยจะสามารถใช้ได้เมื่อเปิดใช้งาน paging` },
+    { require: '', name: 'showCard', type: 'boolean', description: `ใช้สำหรับกำหนดว่าจะให้แสดง card หรือไม่ (card คือ แถบแสดงเลขหน้าปัจจุบันที่ถูกเปิดอยู่ของตาราง) ใช้งานได้เมื่อเปิดใช้งาน paging. มีค่า default เป็น true` },
     { require: '', name: 'cardClass', type: 'string', description: `ใช้สำหรับตั้งค่ารูปแบบ card โดยใช้ class ของ bootstrap สามารถใช้ได้เมื่อ ‘showCard’ ได้เปิดใช้งานเท่านั้น` },
-    { require: '', name: 'filterAll', type: 'boolean', description: `ใช้สำหรับตั้งค่าการแสดงของช่อง input สำหรับกรองข้อมูลทั้งตาราง ซึ่งจะแสดงอยู่ที่ด้านบนของตาราง` },
+    { require: '', name: 'filterAll', type: 'boolean', description: `ใช้สำหรับตั้งค่าการแสดงของช่อง input สำหรับกรองข้อมูลทั้งตาราง ซึ่งจะแสดงอยู่ที่ด้านบนของตาราง มีค่า default เป็น true` },
     { require: '', name: 'columns', type: 'array of object', description: `ใช้สำหรับตั้งค่าข้อมูลของหัวตาราง โดย 1 object จะถือเป็นข้อมูล 1 คอลัมน์` },
     { require: '', name: '--title', type: 'string', description: `ใช้สำหรับกำหนดชื่อที่แสดงบนหัวตาราง` },
     { require: '', name: '--name', type: 'string', description: `ใช้สำหรับกำหนดชื่อตัวแปรของหัวตาราง ซึ่งต้องใช้ในการกำหนดข้อมูลของ data` },
@@ -18,10 +18,12 @@ const ATTRIBUTELIST: Array<any> = [
     { require: '', name: '--dateFormat', type: 'string', description: `ใช้สำหรับกำหนดรูปแบบของวันที่เพื่อใช้ในการแสดง โดยสามารถดูได้จากด้านล่าง หากไม่มีการกำหนดจะถือว่าค่าในคอลัมน์นั้นเป็น string แทน` },
     { require: '', name: '--yearAdditional', type: 'number', description: `ใช้สำหรับเพิ่มจำนวนปีเพื่อใช้ในการแสดงผล โดยค่าเริ่มต้นของจำนวนปีที่แสดงจะเป็นคริสตศักราช (หากต้องการแก้ไขเป็นพุทธศักราชให้ใส่ค่านี้ลงไป 543)` },
     { require: '', name: 'data', type: 'array of object', description: `ใช้สำหรับใส่ข้อมูลลงไปในตารางเพื่อทำการแสดง โดยตัวแปรของแต่ละ object จะต้องตรงกับ column name ที่ได้กำหนดไว้ และหาก object ไหนไม่มีจะถือเป็นข้อมูลว่าง` },
-    { require: '', name: 'useFixedData', type: 'boolean', description: `ใช้สำหรับตั้งค่าให้ตารางแสดงข้อมูลทั้งหมดตามค่า data ที่ใส่เข้าไป เหมาะกับการใช้เพื่อให้มีการควบคุมตารางจากภายนอก` },
-    { require: '', name: 'datalength', type: 'number', description: `ค่านี้จะถูกใช้เมื่อ useFixedData เป็น true เท่านั้น โดยจะเป็นการบังคับกำหนดค่าจำนวนข้อมูลทั้งหมดที่มีเพื่อให้ pagination แสดงผลได้อย่างถูกต้อง` },
+    { require: '', name: 'useFixedData', type: 'boolean', description: `ใช้สำหรับเปิดใช้รูปแบบการแสดงตารางที่รองรับการ query ข้อมูลแบบ fetch data ใช้งานร่วมกับ attribute: datalength, itemPerPage มีค่า default เป็น false 
+    เมื่อเปิดใช้งานแล้ว จะไม่สามารถใช้งาน sorting, filtering แบบปกติได้ แต่ต้องนำค่าที่ได้จาก onTableChanged($event) ไปใช้งานต่อ` },
+    { require: '', name: 'datalength', type: 'number', description: `ค่านี้จะถูกใช้เมื่อ useFixedData เป็น true มีค่า default เท่ากับ 0 ใช้งานร่วมกับ itemPerPage โดยจะนำค่า datalength หารด้วย itemPerPage จะได้จำนวน page ทั้งหมด ซึ่งค่า datalength ควรสัมพันธ์กับจำนวนข้อมูลทั้งหมดที่ query และ itemPerPage
+    ควรสัมพันธ์กับจำนวนข้อมูลที่ fetch มาในแต่ละครั้ง` },
     { require: '', name: 'onCellClicked($event)', type: 'function(event)', description: `ใช้สำหรับรับค่าของช่องเมื่อมีการถูกคลิก` },
-    { require: '', name: 'onTableChanged($event)', type: 'function(event)', description: `ใช้สำหรับรับค่าการเปลี่ยนแปลงเมื่อตารางมีการเปลี่ยนแปลงเกิดขึ้น เช่น มีการเปลี่ยนแปลงลำดับการเรียง เป็นต้น` },
+    { require: '', name: 'onTableChanged($event)', type: 'function(event)', description: `ใช้สำหรับรับค่าการเปลี่ยนแปลงเมื่อตารางมีการเปลี่ยนแปลงเกิดขึ้น เช่น มีการเปลี่ยนแปลงลำดับการเรียง หรือการเปลี่ยนหน้าตาราง เป็นต้น` },
 ];
 
 const DATEFORMATLIST: Array<any> = [
@@ -63,19 +65,56 @@ export class TableDocument {
     private attributeList = ATTRIBUTELIST;
     private dateFormatList = DATEFORMATLIST;
     private introduction_example = "ในตัวอย่างนี้ จะใช้ Data ชุดเดียวกันทั้งหมด ตามด้านล่างนี้ <br>*หมายเหตุ สำหรับช่อง startDate จะมีประเภทของตัวแปร Date";
-    code_1: string = `<go-table class="table table-striped table-border table-hover table-sm" 
-    [paging]=true [itemsPerPage]=10	[maxSize]=3 [showCard]="true" cardClass="card-block card-header card-inverse" 
-    [filterAll]="false" [columns]="columns_1" [data]="data" (onCellClicked)="cellClick($event)" id='table_basic'></go-table>`;
-    code_2: string = `<go-table class="table table-striped table-border table-sm" [paging]=false 
-    [filterAll]=true [columns]="columns_2" [data]="data" id='table_gb_filter'></go-table>`;
-    code_3: string = `<go-table class="table table-striped table-border table-sm table-responsive" 
-    [paging]="true" [itemsPerPage]=4 [maxSize]=2 [showCard]="true" 
-    cardClass="card-block card-header card-inverse" [filterAll]="false" 
-    [columns]="columns_3" [data]="data" id="table_column_filter"></go-table>`;
-    code_4: string = `<go-table class="table table-striped table-border table-sm" [useFixedData]="true" 
-    [paging]="true" [itemsPerPage]=1 [maxSize]=4 [datalength]=6 [showCard]="true" 
-    cardClass="card-block card-header card-inverse" [filterAll]="true" [columns]="columns_4"
-	[data]="data_4" (onTableChanged)="tableChanged($event)" id="table_fetch"></go-table>`;
+    code_1: string = `<strong>Basic and On Cell Clicked</strong><br>
+<go-table id="table_basic" class="table table-striped table-border table-hover table-sm"
+    [filterAll]="false" cardClass="card-block card-header card-inverse" 
+    [columns]="columns_1" [data]="data" (onCellClicked)="cellClick($event)">
+</go-table>
+<span><strong>ช่องที่ถูกคลิก:</strong> {{cellClicked}}</span>`;
+    code_2: string = `<strong>Sorting and Date</strong><br>
+<go-table  id="table_sorting" class="table table-striped table-border table-sm" 
+    [paging]="false" [filterAll]="false" [columns]="columns_2" [data]="data">
+</go-table>`;
+    code_3: string = `<strong>Filter</strong><br>
+<go-table id="table_filter" class="table table-striped table-border table-sm table-responsive" 
+    [itemsPerPage]=4 cardClass="card-block card-header card-inverse" 
+    [columns]="columns_3" [data]="data" >
+</go-table>`;
+    code_4: string = `<strong>Fetch Data and Max Size</strong><br>
+<go-table  id="table_fetch" class="table table-striped table-border table-sm" 
+    [useFixedData]="true" [datalength]=TotalRows [maxSize]=2 [itemsPerPage]=rowPerPage
+    [columns]="columns_4" [data]="data_4" (onTableChanged)="tableChanged($event)">
+</go-table>
+<table class="table table-striped table-border table-sm table-responsive">
+    <tr>
+        <th></th>
+        <th>ค่า filter</th>
+        <th>ค่า sort</th>
+    </tr>
+    <tr>
+        <td>รวมทุกคอลัมน์</td>
+        <td>{{filterAll}}</td>
+    </tr>
+    <tr>
+        <td>คอลัมน์ 1</td>
+        <td>{{filter_1}}</td>
+        <td>{{sort_1}}</td>
+    </tr>
+    <tr>
+        <td>คอลัมน์ 2</td>
+        <td>{{filter_2}}</td>
+        <td>{{sort_2}}</td>
+    </tr>
+    <tr>
+        <td>คอลัมน์ 3</td>
+        <td>{{filter_3}}</td>
+        <td>{{sort_3}}</td>
+    </tr>
+    <tr>
+        <td>ค่า page</td>
+        <td>{{page}}</td>
+    </tr>
+</table>`;
     data_code: string = `    data: Array<any> = [
         { 'name': 'Abc', 'surname': '<b>Cba</b>', 'no': '01', 'startDate': new Date('2015/1/1') },
         { 'name': 'Bar', 'surname': '<i>Foo</i>', 'no': '02', 'startDate': new Date('2015/2/1') },
@@ -85,76 +124,86 @@ export class TableDocument {
         { 'name': 'Ccc', 'surname': 'Ccc', 'no': '06', 'startDate': new Date('2015/7/8') }
     ];`;
     column_1_code: string = `columns_1: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { placeholder: 'Filter by name' } },
-        { title: 'Surname', name: 'surname', sort: false }
-    ];
-    
-    private click:string;
-    cellClick(event: any) {
-        this.click = String(event['row'][event.column]);
-    }`;
+    { title: 'Name', name: 'name', sort: false },
+    { title: 'Surname', name: 'surname', sort: false }
+];
+
+cellClicked: string;
+cellClick(event: any) {
+    this.cellClicked = String(event['row'][event.column]);
+}`;
     column_2_code: string = `columns_2: Array<any> = [
-        { title: 'Name', name: 'name' },
-        { title: 'Id', name: 'no', sort: 'asc' },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'dd-mm-yy' }
-    ]; `;
+    { title: 'Name', name: 'name'},
+    { title: 'Id', name: 'no'},
+    { title: 'Start Date', name: 'startDate', dateFormat: 'dd-mm-yy'}
+];`;
     column_3_code: string = `columns_3: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { placeholder: 'name', filterString: 'Aaa' }, maxWidth: '100px' },
-        { title: 'Id', name: 'no', width: '100px'  },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'mm/dd/yy', yearAdditional: 543, width: '100px'  }
-   ]; `;
-    column_4_code: string =
-    `columns_4: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { filterString: '' } },
-        { title: 'Id', name: 'no' },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'dd/mm/yy', yearAdditional: 543 }
-    ]; 
-    filterAll: string;
-    filter_1: string;
-    filter_2: string;
-    filter_3: string;
-    sort_1: string;
-    sort_2: string;
-    sort_3: string;
-    page: number;
-    data_4:Array<any>;
-     constructor() {
-        this.data_4 = [this.data[0]];
+    { title: 'Name', name: 'name', sort: false, filtering: { placeholder: 'Name'} },
+    { title: 'Id', name: 'no', sort: false , filtering: { placeholder: 'Id'}},
+    { title: 'Start Date', name: 'startDate', dateFormat: 'mm/dd/yy', yearAdditional: 543, filtering: { placeholder: 'Start Date' }, sort: false }
+];`;
+    column_4_code: string =`columns_4: Array<any> = [
+    { title: 'Name', name: 'name', filtering: { placeholder: 'Name'} },
+    { title: 'Id', name: 'no', filtering: { placeholder: 'Id'} },
+    { title: 'Start Date', name: 'startDate', dateFormat: 'dd/mm/yy', yearAdditional: 543, filtering: { placeholder: 'Start Date'} }
+];
+
+data_4: Array<any> = [];
+TotalRows: number = 6;
+rowPerPage: number = 2;
+page: number;
+
+filterAll: string;
+filter_1: string;
+filter_2: string;
+filter_3: string;
+sort_1: string;
+sort_2: string;
+sort_3: string;
+
+constructor() {
+    this.data_4 = this.getDataFirstPage(this.rowPerPage);
+}
+
+//function สมมุติที่มีการทำงานคล้ายการ fetch ข้อมูลรอบแรก
+getDataFirstPage(rowPerPage: number): Array<any> {
+    let data_fetch: Array<any> = [];
+    for (let i = 0; i < rowPerPage; i++) {
+        data_fetch.push(this.data[i]);
     }
-    tableChanged(event: any) {
-        //สำหรับตัวอย่างนี้มี 3 คอลัมน์ ค่า output filter และ sort จึงมี 3 ค่าเช่นกัน
-        this.filterAll = event.filterString; //ค่า output ที่เกิดขึ้นจากการ filter แบบรวม
-        //ค่า output ที่เกิดจากการ filter แบบแยกคอลัมน์
-        this.filter_1 = event.columns[0].filtering ? event.columns[0].filtering.filterString : '';
-        this.filter_2 = event.columns[1].filtering ? event.columns[1].filtering.filterString : '';
-        this.filter_3 = event.columns[2].filtering ? event.columns[2].filtering.filterString : '';
-        //ค่า output ที่เกิดจากการเปลี่ยน sort
-        this.sort_1 = event.columns[0].sort;
-        this.sort_2 = event.columns[1].sort;
-        this.sort_3 = event.columns[2].sort;
-        this.page = event.page; // ค่า output เมื่อมีการเปลี่ยนหน้า page ด้านล่าง
-        //data ที่แสดงใน table จะเปลี่ยนแปลงตามค่า page โดยหากกดที่ page หน้า 2 ก็จะดึงข้อมูลแถวที่ 2 มาแสดง
-        this.data_4 = [this.data[this.page - 1]]; 
-    }`;
-    columns_1: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { placeholder: 'Filter by name' } },
-        { title: 'Surname', name: 'surname', sort: false }
-    ];
-    columns_2: Array<any> = [
-        { title: 'Name', name: 'name' },
-        { title: 'Id', name: 'no', sort: 'asc' },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'dd-mm-yy'}
-    ];
-    columns_3: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { placeholder: 'name', filterString: 'Aaa' }, maxWidth: '100px' },
-        { title: 'Id', name: 'no', width: '100px' },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'mm/dd/yy', yearAdditional: 543, width: '100px' }
-    ];
-    columns_4: Array<any> = [
-        { title: 'Name', name: 'name', filtering: { filterString: '' } },
-        { title: 'Id', name: 'no',filtering: { filterString: '' } },
-        { title: 'Start Date', name: 'startDate', dateFormat: 'dd/mm/yy', yearAdditional: 543 ,filtering: { filterString: '' }}
-    ];
+    return data_fetch;
+}
+
+tableChanged(event: any) {
+    //สำหรับตัวอย่างนี้มี 3 คอลัมน์ ค่า output filter และ sort จึงมี 3 ค่าเช่นกัน
+    this.filterAll = event.filterString; //ค่า output ที่เกิดขึ้นจากการ filter แบบรวม
+
+    //ค่า output ที่เกิดจากการ filter แบบแยกคอลัมน์
+    this.filter_1 = event.columns[0].filtering ? event.columns[0].filtering.filterString : '';
+    this.filter_2 = event.columns[1].filtering ? event.columns[1].filtering.filterString : '';
+    this.filter_3 = event.columns[2].filtering ? event.columns[2].filtering.filterString : '';
+
+    //ค่า output ที่เกิดจากการเปลี่ยน sort
+    this.sort_1 = event.columns[0].sort;
+    this.sort_2 = event.columns[1].sort;
+    this.sort_3 = event.columns[2].sort;
+
+    // ค่า output เลข page ปัจจุบัน เมื่อกดปุ่มเปลี่ยน page
+    this.page = event.page;
+
+    //การสมมุติการ fetch data จาก db มาเก็บในตัวแปร data_4
+    this.data_4 = this.fetchNextNRows(this.rowPerPage, this.page);
+}
+
+//function สมมุติที่มีการทำงานคล้ายการ fetch ข้อมูล
+fetchNextNRows(rowPerPage: number, page: number): Array<any> {
+    let lastRowOfFetchRound = rowPerPage * page;
+    let data_fetch: Array<any> = [];
+    for (let i = lastRowOfFetchRound - this.rowPerPage; i < lastRowOfFetchRound; i++) {
+        data_fetch.push(this.data[i]);
+    }
+    return data_fetch;
+}`;
     data: Array<any> = [
         { 'name': 'Abc', 'surname': '<b>Cba</b>', 'no': '01', 'startDate': new Date('2015/1/1') },
         { 'name': 'Bar', 'surname': '<i>Foo</i>', 'no': '02', 'startDate': new Date('2015/2/1') },
@@ -163,13 +212,36 @@ export class TableDocument {
         { 'name': 'Bbb', 'no': '05', },
         { 'name': 'Ccc', 'surname': 'Ccc', 'no': '06', 'startDate': new Date('2015/7/8') }
     ];
-    click: string;
+    columns_1: Array<any> = [
+        { title: 'Name', name: 'name', sort: false },
+        { title: 'Surname', name: 'surname', sort: false }
+    ];
+    columns_2: Array<any> = [
+        { title: 'Name', name: 'name'},
+        { title: 'Id', name: 'no'},
+        { title: 'Start Date', name: 'startDate', dateFormat: 'dd-mm-yy'}
+    ];
+    columns_3: Array<any> = [
+        { title: 'Name', name: 'name', sort: false, filtering: { placeholder: 'Name'} },
+        { title: 'Id', name: 'no', sort: false , filtering: { placeholder: 'Id'}},
+        { title: 'Start Date', name: 'startDate', dateFormat: 'mm/dd/yy', yearAdditional: 543, filtering: { placeholder: 'Start Date' }, sort: false }
+    ];
+    columns_4: Array<any> = [
+        { title: 'Name', name: 'name', filtering: { placeholder: 'Name'} },
+        { title: 'Id', name: 'no', filtering: { placeholder: 'Id'} },
+        { title: 'Start Date', name: 'startDate', dateFormat: 'dd/mm/yy', yearAdditional: 543, filtering: { placeholder: 'Start Date'} }
+    ];
+
+    cellClicked: string;
     cellClick(event: any) {
-        this.click = String(event['row'][event.column]);
+        this.cellClicked = String(event['row'][event.column]);
     }
-    constructor() {
-        this.data_4 = [this.data[0]];
-    }
+
+    data_4: Array<any> = [];
+    TotalRows: number = 6;
+    rowPerPage: number = 2;
+    page: number;
+    
     filterAll: string;
     filter_1: string;
     filter_2: string;
@@ -177,22 +249,48 @@ export class TableDocument {
     sort_1: string;
     sort_2: string;
     sort_3: string;
-    page: number;
-    data_4:Array<any>;
+
+    constructor() {
+        this.data_4 = this.getDataFirstPage(this.rowPerPage);
+    }
+
+    //function สมมุติที่มีการทำงานคล้ายการ fetch ข้อมูลรอบแรก
+    getDataFirstPage(rowPerPage: number): Array<any> {
+        let data_fetch: Array<any> = [];
+        for (let i = 0; i < rowPerPage; i++) {
+            data_fetch.push(this.data[i]);
+        }
+        return data_fetch;
+    }
+
     tableChanged(event: any) {
         //สำหรับตัวอย่างนี้มี 3 คอลัมน์ ค่า output filter และ sort จึงมี 3 ค่าเช่นกัน
         this.filterAll = event.filterString; //ค่า output ที่เกิดขึ้นจากการ filter แบบรวม
+
         //ค่า output ที่เกิดจากการ filter แบบแยกคอลัมน์
         this.filter_1 = event.columns[0].filtering ? event.columns[0].filtering.filterString : '';
         this.filter_2 = event.columns[1].filtering ? event.columns[1].filtering.filterString : '';
         this.filter_3 = event.columns[2].filtering ? event.columns[2].filtering.filterString : '';
+
         //ค่า output ที่เกิดจากการเปลี่ยน sort
         this.sort_1 = event.columns[0].sort;
         this.sort_2 = event.columns[1].sort;
         this.sort_3 = event.columns[2].sort;
-        this.page = event.page; // ค่า output เมื่อมีการเปลี่ยนหน้า page ด้านล่าง
-        //data ที่แสดงใน table จะเปลี่ยนแปลงตามค่า page โดยหากกดที่ page หน้า 2 ก็จะดึงข้อมูลแถวที่ 2 มาแสดง
-        this.data_4 = [this.data[this.page - 1]];
+
+        // ค่า output เลข page ปัจจุบัน เมื่อกดปุ่มเปลี่ยน page
+        this.page = event.page;
+
+        //การสมมุติการ fetch data จาก db มาเก็บในตัวแปร data_4
+        this.data_4 = this.fetchNextNRows(this.rowPerPage, this.page);
     }
 
+    //function สมมุติที่มีการทำงานคล้ายการ fetch ข้อมูล
+    fetchNextNRows(rowPerPage: number, page: number): Array<any> {
+        let lastRowOfFetchRound = rowPerPage * page;
+        let data_fetch: Array<any> = [];
+        for (let i = lastRowOfFetchRound - this.rowPerPage; i < lastRowOfFetchRound; i++) {
+            data_fetch.push(this.data[i]);
+        }
+        return data_fetch;
+    }
 }
